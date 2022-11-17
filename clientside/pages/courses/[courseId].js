0b4a409fc030514b507ec88e666cf22d320ec157
Bodi Augustus <bodiAugustus@protonmail.com>
2022-11-17 12:14:44 +0200

    changed dates

--- clientside/pages/courses/[courseId].js
+++ clientside/pages/courses/[courseId].js
@@ -57,28 +57,28 @@ function CourseDetailPage(props) {
                   lineItems: [
                     {
                       price: `${
-                        course.title === 'React' && course.date === '2023-01-07'
+                        course.title === 'React' && course.date === '2023-01-14'
                           ? 'price_1M4z7GFSa0TezXP7VGtkXIOq'
                           : course.title === 'React' &&
-                            course.date === '2023-01-08'
+                            course.date === '2023-01-22'
                           ? 'price_1M4z6oFSa0TezXP73mvaPHE1'
                           : course.title === 'JavaScript' &&
-                            course.date === '2023-01-07'
+                            course.date === '2023-01-14'
                           ? 'price_1M4z5UFSa0TezXP76X4ZTn3N'
                           : course.title === 'JavaScript' &&
-                            course.date === '2023-01-08'
+                            course.date === '2023-01-22'
                           ? 'price_1M4z4mFSa0TezXP7BpXKkIgC'
                           : course.title === 'JavaScript' &&
-                            course.date === '2023-01-13'
+                            course.date === '2023-01-27'
                           ? 'price_1M4z3jFSa0TezXP7v76Gh5Ev'
                           : course.title === 'HTML & CSS' &&
-                            course.date === '2023-01-07'
+                            course.date === '2023-01-14'
                           ? 'price_1M4z1WFSa0TezXP7oYUz8M3W'
                           : course.title === 'HTML & CSS' &&
-                            course.date === '2023-01-08'
+                            course.date === '2023-01-22'
                           ? 'price_1M4z0HFSa0TezXP7Enl8oiOQ'
                           : course.title === 'HTML & CSS' &&
-                            course.date === '2023-01-11'
+                            course.date === '2023-01-26'
                           ? 'price_1M4yzGFSa0TezXP7buRLb6Uv'
                           : 'price_1M4yyAFSa0TezXP71Q6t7fr4'
                       }`,
@@ -128,28 +128,28 @@ function CourseDetailPage(props) {
                   lineItems: [
                     {
                       price: `${
-                        course.title === 'React' && course.date === '2023-01-07'
+                        course.title === 'React' && course.date === '2023-01-14'
                           ? 'price_1M4z7GFSa0TezXP7VGtkXIOq'
                           : course.title === 'React' &&
-                            course.date === '2023-01-08'
+                            course.date === '2023-01-22'
                           ? 'price_1M4z6oFSa0TezXP73mvaPHE1'
                           : course.title === 'JavaScript' &&
-                            course.date === '2023-01-07'
+                            course.date === '2023-01-14'
                           ? 'price_1M4z5UFSa0TezXP76X4ZTn3N'
                           : course.title === 'JavaScript' &&
-                            course.date === '2023-01-08'
+                            course.date === '2023-01-22'
                           ? 'price_1M4y7CFSa0TezXP77P272r3A'
                           : course.title === 'JavaScript' &&
-                            course.date === '2023-01-13'
+                            course.date === '2023-01-27'
                           ? 'price_1M4z3jFSa0TezXP7v76Gh5Ev'
                           : course.title === 'HTML & CSS' &&
-                            course.date === '2023-01-07'
+                            course.date === '2023-01-14'
                           ? 'price_1M4z1WFSa0TezXP7oYUz8M3W'
                           : course.title === 'HTML & CSS' &&
-                            course.date === '2023-01-08'
+                            course.date === '2023-01-22'
                           ? 'price_1M4z0HFSa0TezXP7Enl8oiOQ'
                           : course.title === 'HTML & CSS' &&
-                            course.date === '2023-01-11'
+                            course.date === '2023-01-26'
                           ? 'price_1M4yzGFSa0TezXP7buRLb6Uv'
                           : 'price_1M4yyAFSa0TezXP71Q6t7fr4'
                       }`,
